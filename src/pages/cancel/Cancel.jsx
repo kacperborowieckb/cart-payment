fb595752aed4bc3e4c75032727623e4c09727290
kacperborowieckb <kacperborowiec.kb@gmail.com>
2023-05-12 15:06:16 +0200

    added succes/cancel page design

--- src/pages/cancel/Cancel.jsx
+++ src/pages/cancel/Cancel.jsx
@@ -1,7 +1,15 @@
+import { Link } from 'react-router-dom';
 import './cancel.scss';
 
 const Cancel = () => {
-  return <div>Cancel</div>;
+  return (
+    <div className="cancel">
+      <h1 className="cancel__header">Payment has been canceled.</h1>
+      <Link to={'/'}>
+        <button className="cancel__button">Go Home</button>
+      </Link>
+    </div>
+  );
 };
 
 export default Cancel;
--- src/pages/success/Success.jsx
+++ src/pages/success/Success.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect } from 'react';
 import './success.scss';
 import DataContext from '../../context/DataContext';
+import { Link } from 'react-router-dom';
 
 const Succes = () => {
   const { setCartItems } = useContext(DataContext);
@@ -10,7 +11,14 @@ const Succes = () => {
     localStorage.setItem('cartItems', JSON.stringify([]));
   }, []);
 
-  return <div>Succes</div>;
+  return (
+    <div className="success">
+      <h1 className="success__header">Success!</h1>
+      <Link to={'/'}>
+        <button className="success__button">Go Home</button>
+      </Link>
+    </div>
+  );
 };
 
 export default Succes;
